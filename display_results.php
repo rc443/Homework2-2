c3b0b89ccbae2934404d03a6ed3652120bc55c36
Rafael Correa <rc443@njit.edu>
2017-06-21 21:43:57 -0400

    fixed some php errors in my code

--- display_results.php
+++ display_results.php
@@ -37,7 +37,7 @@
    
    //calculate the future value
    $future_value = $investment;
-   for ($1 = 1; $i <= $years; $i++) {
+   for ($i = 1; $i <= $years; $i++) {
    	$future_value = $future_value + ($future_value * $interest_rate *.01);
    }
    
@@ -58,13 +58,13 @@
 		<h1>Future Value Calculator</h1>
 
 		<label> Investment Amount:</label>
-		<span><?php echo $investment_f; ?></span><br>
+		<span><?php echo htmlspecialchars($investment_f); ?></span><br>
 
 		<label>Yearly Interest Rate:</label>
-		<span><?php echo $yearly_rate_f; ?></span><br>
+		<span><?php echo htmlspecialchars($yearly_rate_f); ?></span><br>
 
 		<label>Number of Years:</label>
-		<span><?php echo $future_value_f; ?></span><br>
+		<span><?php echo htmlspecialchars($years); ?></span><br>
 
 		<label>Future Value:</label>
 		<span><?php echo htmlspecialchars($future_value_f); ?></span><br>
